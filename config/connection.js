aab9db4f6354b0bea498fb8d2502eb828570f891
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-22 18:16:06 -0400

    Troubleshooting Heroku DB deployment.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,37 @@
 // export the mySQL connection
+/*
 module.exports = require("mysql").createConnection({
   host: 'us-cdbr-iron-east-05.cleardb.net',
   user: 'b05906cd406f22',
   password: '09943536',
   database: 'heroku_4fce34ca6fbe329'
 });
+*/
 
 //mysql://b05906cd406f22:09943536@us-cdbr-iron-east-05.cleardb.net/heroku_4fce34ca6fbe329?reconnect=true
+
+var mysql = require('mysql');
+
+var connection;
+
+if (process.env.CLEARDB_DATABASE_URL) {
+	connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL);
+} else {
+	connection = mysql.createConnection({
+		port: 3306,
+		host: 'localhost',
+		user: 'root',
+		password: 'root',
+		database: 'burgers_db'
+	})
+};
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('ERROR: MySQL connection error -- ' + err.stack + '\n\n');
+    return;
+  }
+  console.log('Connected to MySQL database as id ' + connection.threadId + '\n\n');
+});
+
+module.exports = connection;
