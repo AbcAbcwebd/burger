815054090f59f6f9c42f37c7cfcc950be24e8d63
Owen Essen <abc_abcwebd@yahoo.com>
2017-08-22 17:52:56 -0400

    Heroku DB command line troubleshooting.

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,9 @@
 // export the mySQL connection
 module.exports = require("mysql").createConnection({
-  host     : 'us-cdbr-iron-east-05.cleardb.net',
-  user     : 'b05906cd406f22',
-  password : '09943536',
-  database : 'heroku_4fce34ca6fbe329'
+  host: 'us-cdbr-iron-east-05.cleardb.net',
+  user: 'b05906cd406f22',
+  password: '09943536',
+  database: 'heroku_4fce34ca6fbe329'
 });
 
 //mysql://b05906cd406f22:09943536@us-cdbr-iron-east-05.cleardb.net/heroku_4fce34ca6fbe329?reconnect=true
